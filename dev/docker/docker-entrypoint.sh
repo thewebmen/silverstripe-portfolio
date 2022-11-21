136fe80342c6569acc55cbe80af6acba671bbd4a
Mirjana Durakovic <mirjana@webmen.nl>
2022-11-21 14:33:47 +0100

    fix docker-entrypoint

--- dev/docker/docker-entrypoint.sh
+++ dev/docker/docker-entrypoint.sh
@@ -8,6 +8,8 @@ fi
 
 if [ "$1" = 'php' ]; then
     composer install --prefer-dist --no-progress --no-suggest --no-interaction --no-plugins --ignore-platform-reqs
+    yarn install
+    echo "Container is ready!"
 fi
 
 exec docker-php-entrypoint "$@"
